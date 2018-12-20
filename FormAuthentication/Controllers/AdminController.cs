af4399165796aea94298bd074c2a288d6ce554a2
ajit <ajitagawal89@outlook.com>
2018-12-20 10:05:19 +0530

    Add project files.

---
+++ FormAuthentication/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+namespace FormAuthentication.Controllers
+{
+    public class AdminController : Controller
+    {
+        DAL.dbContext db = new DAL.dbContext();
+        // GET: Admin
+        public ActionResult Admin_Login()
+        { return View(); }
+        [HttpPost]
+        public ActionResult Admin_Login(FormCollection fc)
+        {
+            int res = db.Admin_Login(fc["adminid"], fc["Password"]);
+            if (res == 1)
+            {
+                FormsAuthentication.SetAuthCookie(fc["Admin_Id"],true);
+                return RedirectToAction("Profile");
+            }
+            else {
+                TempData["msg"] = "Admin Id and Password is wrong...";
+            }
+            return View();
+        }
+        [Authorize]
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return View();
+        }
+    }
+}
---
+++ FormAuthentication/DAL/dbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace FormAuthentication.DAL
+{
+    public class dbContext
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        public int Admin_Login(string Admin_Id, string Password)
+        {
+            SqlCommand cmd = new SqlCommand("Sp_Admin_Login", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Admin_Id", Admin_Id);
+            cmd.Parameters.AddWithValue("@Password", Password);
+            SqlParameter sqlParameter = new SqlParameter();
+            sqlParameter.ParameterName = "@Isvalid";
+            sqlParameter.Direction = ParameterDirection.Output;
+            sqlParameter.SqlDbType = SqlDbType.Bit;
+            cmd.Parameters.Add(sqlParameter);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            int rest = Convert.ToInt32(sqlParameter.Value);
+            con.Close();
+            return rest;
+          
+        }
+    }
+
+}
